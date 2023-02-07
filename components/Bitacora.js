c99a59bc1b04235aa1ee6ecb05245a7ed140bbae
nicolasrm-droid <72952718+nicolasrm-droid@users.noreply.github.com>
2023-02-07 00:22:19 -0500

    Add files via upload

--- components/Bitacora.js
+++ components/Bitacora.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
 import Heading from './Heading'
 
 const Bitacora = () => {
   return (
-    <Heading title="Bitacora"/>
+    <div className='img1 bg-[#0b1011] ' >
+      <Heading title="Bitacora - Arduino1" />
+      <div >
+      <img id="imagen1" src="imagen 1.jpeg" alt="Arduino 1"></img>
+      </div>
+      <div>
+        <img id="imagen2" src="imagen2.jpeg" alt="Arduino 1"></img>
+      </div>
+      <div id='video' alt="video">
+        <video controls type="video.mp4"> 
+          <source src="video.mp4" />
+        </video>
+      </div>
+      <div className='py-4 px-2 text-center '>
+        <p>En esta primera sesion no enseñaron que es un Arduino como conectarlo a la protoboard hicimos un ejercios con leds 
+          como queda registrado en en video 
+        </p>
+      </div>
+    </div>
+
+
+
+
+
+
   )
 }
 
--- components/Titulo.js
+++ components/Titulo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Titulo = () => {
     return (
-        <div className='view bg-[#011523] md:h-screen bg-cover bg-center bg-local flex item-center'>
+        <div className='view bg-[#ffffff] md:h-screen bg-cover bg-center bg-local flex item-center'>
             <div className='container mx-auto px-4  flex justify-center items-center h-screen ' >
                 <div className='max-w-[450px] flex flex-col gap-[40px] '>
                     <div>
